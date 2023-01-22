0470f502cab5808ad138a530b487f5b69d23346a
MOFIJUL HAQUE <mofijulhoque995@gmail.com>
2023-01-23 01:05:20 +0530

    22-01-23

--- Practice/Promise/index.js
+++ Practice/Promise/index.js
@@ -1,11 +1,10 @@
 const pr = new Promise(function (resolve, reject) {
-  const data = true;
   setTimeout(() => {
     resolve("success ---->");
   }, 2000);
 });
 
 pr.then(function
-   (result) {
+  (result) {
   console.log(result, "Print after resolve the promise");
 });
